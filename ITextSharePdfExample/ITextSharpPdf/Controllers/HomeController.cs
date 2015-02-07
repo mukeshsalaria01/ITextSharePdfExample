6bf08e89488f5eaa3d749b5f24c250f7639a71c1
Mukesh Salaria <mukeshsalaria01@gmail.com>
2015-02-07 17:01:17 +0530

    First Commit
    Example to generate pdf using asp.net mvc c# with ITextSharp

---
+++ ITextSharePdfExample/ITextSharpPdf/Controllers/HomeController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using WPW.Web.UI;
+
+namespace ITextSharpPdf.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Generate Pdf using ITextSharp using asp.net mvc c#";
+
+            return View();
+        }
+
+        public ActionResult GeneratePdf()
+        {
+            //var userTickets = JsonConvert.DeserializeObject<List<TicketsByNameDetails>>(jsonString);
+
+            var doc = new Document(PageSize.A4, 10f, 10f, 120f, 100f);
+            var strFilePath = Server.MapPath("~/PdfUploads/");
+
+            var fileName = "Pdf_" + DateTime.Now.ToString("yyyyMMddHHmmssffff") + ".pdf";
+
+            var titleFont = FontFactory.GetFont(FontFactory.HELVETICA, 9, Font.UNDERLINE, BaseColor.BLACK);
+            var h1Font = FontFactory.GetFont(FontFactory.HELVETICA, 11, Font.NORMAL);
+            var bodyFont = FontFactory.GetFont(FontFactory.HELVETICA, 9, Font.NORMAL, BaseColor.DARK_GRAY);
+
+            try
+            {
+                var pdfWriter = PdfWriter.GetInstance(doc, new FileStream(strFilePath + fileName, FileMode.Create));
+                pdfWriter.PageEvent = new ITextEvents();
+                doc.Open();
+
+                var tblContainer = new PdfPTable(5) { TotalWidth = 520f, LockedWidth = true };
+                float[] widths = { 90f, 150f, 120f, 95f, 65f };
+                tblContainer.SetWidths(widths);
+                var heading = new Phrase("LearnShareCorner demo code to Genereate Pdf using ITextSharp.", h1Font);
+                var titleEmployee = new Phrase("Employee", titleFont);
+                var titleName = new Phrase("Name", titleFont);
+                var titleOccupation = new Phrase("Occupation", titleFont);
+                var titleLa = new Phrase("Lapse Action", titleFont);
+                var titleExpiryDate = new Phrase("Expiry Date", titleFont);
+                var cellTicketName = new PdfPCell(heading) { Colspan = 5, Border = 0 };
+                var cellTitleEmployee = new PdfPCell(titleEmployee);
+                var cellTitleName = new PdfPCell(titleName);
+                var cellTitleOccupation = new PdfPCell(titleOccupation);
+                var cellTitleLa = new PdfPCell(titleLa);
+                var cellTitleExpiryDate = new PdfPCell(titleExpiryDate);
+
+                cellTitleEmployee.Border = 0;
+                cellTitleName.Border = 0;
+                cellTitleOccupation.Border = 0;
+                cellTitleLa.Border = 0;
+                cellTitleExpiryDate.Border = 0;
+
+                tblContainer.AddCell(cellTicketName);
+
+                tblContainer.AddCell(cellTitleEmployee);
+                tblContainer.AddCell(cellTitleName);
+                tblContainer.AddCell(cellTitleOccupation);
+                tblContainer.AddCell(cellTitleLa);
+                tblContainer.AddCell(cellTitleExpiryDate);
+
+                doc.Add(tblContainer);
+
+                var tblResult = new PdfPTable(5) { TotalWidth = 520f, LockedWidth = true };
+                tblResult.SetWidths(widths);
+                var employee = new Phrase("WebTechSys.in", bodyFont);
+                var name = new Phrase("Mukesh Salaria", bodyFont);
+                var occupation = new Phrase("Software Engineer", bodyFont);
+                var la = new Phrase("None",bodyFont);
+
+                var expiryDate =  new Phrase("N/A", bodyFont);
+                var cellEmployee = new PdfPCell(employee);
+                var cellName = new PdfPCell(name);
+                var cellOccupation = new PdfPCell(occupation);
+                var cellLa = new PdfPCell(la);
+                var cellExpiryDate = new PdfPCell(expiryDate);
+
+                cellEmployee.Border = 0;
+                cellName.Border = 0;
+                cellOccupation.Border = 0;
+                cellLa.Border = 0;
+                cellExpiryDate.Border = 0;
+                tblResult.AddCell(cellEmployee);
+                tblResult.AddCell(cellName);
+                tblResult.AddCell(cellOccupation);
+                tblResult.AddCell(cellLa);
+                tblResult.AddCell(cellExpiryDate);
+
+                doc.Add(tblResult);
+
+                doc.Close();
+                //return Json(new { success = "true", link = strFilePath + fileName });
+                byte[] contents = System.IO.File.ReadAllBytes(strFilePath + fileName);
+                return File(contents, "application/pdf", fileName);
+            }
+            catch (Exception ex)
+            {
+                return HandleErrorCondition(ex.Message);
+            }
+            finally
+            {
+                doc.Close();
+            }
+        }
+
+        private ActionResult HandleErrorCondition(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ITextSharePdfExample/ITextSharpPdf/ITextEvents.cs
@@ -0,0 +1,217 @@
+﻿using System.Web;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+
+namespace WPW.Web.UI
+{
+    public class ITextEvents : PdfPageEventHelper
+    {
+        // This is the contentbyte object of the writer
+        PdfContentByte cb;
+
+        // we will put the final number of pages in a template
+        PdfTemplate headerTemplate, footerTemplate;
+
+        // this is the BaseFont we are going to use for the header / footer
+        BaseFont bf = null;
+
+        // This keeps track of the creation time
+        DateTime PrintTime = DateTime.Now;
+
+
+        #region Fields
+        private string _header;
+        #endregion
+
+        #region Properties
+        public string Header
+        {
+            get { return _header; }
+            set { _header = value; }
+        }
+        #endregion
+
+
+        public override void OnOpenDocument(PdfWriter writer, Document document)
+        {
+            try
+            {
+                PrintTime = DateTime.Now;
+                bf = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+                cb = writer.DirectContent;
+                headerTemplate = cb.CreateTemplate(400, 150);
+                footerTemplate = cb.CreateTemplate(50, 50);
+            }
+            catch (DocumentException de)
+            {
+
+            }
+            catch (System.IO.IOException ioe)
+            {
+
+            }
+        }
+
+        public override void OnEndPage(PdfWriter writer, Document document)
+        {
+            base.OnEndPage(writer, document);
+
+            Font baseFontNormal = FontFactory.GetFont(FontFactory.HELVETICA, 12f, Font.NORMAL, BaseColor.BLACK);
+
+            Font baseFontBig = FontFactory.GetFont(FontFactory.TIMES, 12f, Font.NORMAL, BaseColor.BLACK);
+            Font time = FontFactory.GetFont(FontFactory.HELVETICA, 11f, Font.NORMAL);
+
+            var logo = Image.GetInstance(HttpContext.Current.Server.MapPath("~/Images/LSC.png"));
+            logo.ScaleToFit(150f, 62f);
+            //document.Add(new Phrase(Environment.NewLine));
+            var p1Header = new Phrase("\"LearnShareCorner is platform where you can learn and share your knowledge\"", baseFontNormal);
+
+            //Create PdfTable object
+            var pdfTab = new PdfPTable(3);
+            float[] width = { 100f, 320f,100f };
+            pdfTab.SetWidths(width);
+            pdfTab.TotalWidth = 520f;
+            pdfTab.LockedWidth = true;
+            //We will have to create separate cells to include image logo and 2 separate strings
+            //Row 1
+            var pdfCell1 = new PdfPCell(logo);
+            var pdfCell2 = new PdfPCell(p1Header);
+            var text = "Page " + writer.PageNumber + " of ";
+            //string
+
+            //Add paging to header
+            {
+                cb.BeginText();
+                cb.SetFontAndSize(bf, 12);
+                cb.SetTextMatrix(document.PageSize.GetRight(100), document.PageSize.GetTop(45));
+                cb.ShowText(text);
+                cb.EndText();
+                float len = bf.GetWidthPoint(text, 12);
+                //Adds "12" in Page 1 of 12
+                cb.AddTemplate(headerTemplate, document.PageSize.GetRight(100) + len, document.PageSize.GetTop(45));
+            }
+            //Add paging to footer
+            {
+                var leftCol = new Paragraph("Mukesh Salaria\n" + "Software Engineer", time);
+                var rightCol = new Paragraph("LearnShareCorner.com\n" + "Techical Blog", time);
+                var phone = new Paragraph("Phone +91-9814268272", time);
+                var address = new Paragraph("       Company: webtechsys.in\n" + "            SAS Nagar, Mohali", time);
+                var fax = new Paragraph("mukeshsalaria01@gmail.com", time);
+
+                leftCol.Alignment = Element.ALIGN_LEFT;
+                rightCol.Alignment = Element.ALIGN_RIGHT;
+                fax.Alignment = Element.ALIGN_RIGHT;
+                phone.Alignment = Element.ALIGN_LEFT;
+                address.Alignment = Element.ALIGN_CENTER;
+
+                var footerTbl = new PdfPTable(3) { TotalWidth = 520f, HorizontalAlignment = Element.ALIGN_CENTER, LockedWidth = true };
+                float[] widths = { 150f, 220f, 150f };
+                footerTbl.SetWidths(widths);
+                var footerCell1 = new PdfPCell(leftCol);
+                var footerCell2 = new PdfPCell();
+                var footerCell3 = new PdfPCell(rightCol);
+                var sep = new PdfPCell();
+                var footerCell4 = new PdfPCell(phone);
+                var footerCell5 = new PdfPCell(address);
+                var footerCell6 = new PdfPCell(fax);
+
+
+                footerCell1.Border = 0;
+                footerCell2.Border = 0;
+                footerCell3.Border = 0;
+                footerCell4.Border = 0;
+                footerCell5.Border = 0;
+                footerCell6.Border = 0;
+                footerCell6.HorizontalAlignment = Element.ALIGN_RIGHT;
+                sep.Border = 0;
+                sep.FixedHeight = 10f;
+                footerCell3.HorizontalAlignment = Element.ALIGN_RIGHT;
+                footerCell6.PaddingLeft = 0;
+                sep.Colspan = 3;
+
+                footerTbl.AddCell(footerCell1);
+                footerTbl.AddCell(footerCell2);
+                footerTbl.AddCell(footerCell3);
+                footerTbl.AddCell(sep);
+                footerTbl.AddCell(footerCell4);
+                footerTbl.AddCell(footerCell5);
+                footerTbl.AddCell(footerCell6);
+                footerTbl.WriteSelectedRows(0, -1, 40, 80, writer.DirectContent);
+            }
+            //Row 2
+            // PdfPCell pdfCell4 = new PdfPCell(new Phrase("No job is so urgent that it cannot be done safely", baseFontNormal));
+            //Row 3
+
+            var pdfCell3 = new PdfPCell(new Phrase("Date:" + PrintTime.ToShortDateString(), baseFontBig));
+            var pdfCell4 = new PdfPCell();
+            //var pdfCell5 = new PdfPCell(new Phrase("TIME:" + string.Format("{0:t}", DateTime.Now), baseFontBig));
+            var pdfCell5 = new PdfPCell(new Phrase(""));
+
+            //set the alignment of all three cells and set border to 0
+            pdfCell1.HorizontalAlignment = Element.ALIGN_LEFT;
+            pdfCell2.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell3.HorizontalAlignment = Element.ALIGN_RIGHT;
+            pdfCell5.HorizontalAlignment = Element.ALIGN_RIGHT;
+
+            //pdfCell2.VerticalAlignment = Element.ALIGN_BOTTOM;
+
+            //pdfCell1.Colspan = 3;
+            //pdfCell2.Colspan = 3;
+            pdfCell2.PaddingTop = 9f;
+            pdfCell3.PaddingTop = 20f;
+            pdfCell5.PaddingTop = 9f;
+
+            pdfCell1.Border = 0;
+            pdfCell2.Border = 0;
+            pdfCell3.Border = 0;
+            pdfCell4.Border = 0;
+            pdfCell5.Border = 0;
+
+            //add all three cells into PdfTable
+            pdfTab.AddCell(pdfCell1);
+            pdfTab.AddCell(pdfCell2);
+            pdfTab.AddCell(pdfCell3);
+            pdfTab.AddCell(pdfCell4);
+            pdfTab.AddCell(pdfCell5);
+
+            pdfTab.TotalWidth = 520f;
+            pdfTab.LockedWidth = true;
+            //pdfTab.TotalWidth = document.PageSize.Width;
+            //pdfTab.WidthPercentage = 100;
+
+            //call WriteSelectedRows of PdfTable. This writes rows from PdfWriter in PdfTable
+            //first param is start row. -1 indicates there is no end row and all the rows to be included to write
+            //Third and fourth param is x and y position to start writing
+            pdfTab.WriteSelectedRows(0, -1, 40, document.PageSize.Height - 30, writer.DirectContent);
+            //set pdfContent value
+
+            //Move the pointer and draw line to separate header section from rest of page
+            cb.MoveTo(40, document.PageSize.Height - 100);
+            cb.LineTo(document.PageSize.Width - 40, document.PageSize.Height - 100);
+            cb.Stroke();
+
+            //Move the pointer and draw line to separate footer section from rest of page
+            cb.MoveTo(40, document.PageSize.GetBottom(50));
+            cb.LineTo(document.PageSize.Width - 40, document.PageSize.GetBottom(50));
+            cb.Stroke();
+        }
+
+        public override void OnCloseDocument(PdfWriter writer, Document document)
+        {
+            base.OnCloseDocument(writer, document);
+
+            headerTemplate.BeginText();
+            headerTemplate.SetFontAndSize(bf, 12);
+            headerTemplate.SetTextMatrix(0, 0);
+            headerTemplate.ShowText((writer.PageNumber - 1).ToString());
+            headerTemplate.EndText();
+
+            footerTemplate.BeginText();
+            footerTemplate.SetFontAndSize(bf, 12);
+            footerTemplate.SetTextMatrix(0, 0);
+            footerTemplate.ShowText((writer.PageNumber - 1).ToString());
+            footerTemplate.EndText();
+        }
+    }
+}
